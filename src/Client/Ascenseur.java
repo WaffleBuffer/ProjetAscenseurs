38867bff95a26f464e35ef8460049916737274dd
s14004402 <s14004402@a-f001809-l.salsa.univ-amu.fr>
2016-01-07 10:00:16 +0100

    Initialisation boutons corrections erreurs dans Ascenceur

--- src/Client/Ascenseur.java
+++ src/Client/Ascenseur.java
@@ -3,6 +3,7 @@ package Client;
 import java.util.ArrayList;
 
 import Boutons.Bouton;
+import Boutons.BoutonDestination;
 import Boutons.BoutonInterne;
 
 
@@ -12,7 +13,7 @@ public class Ascenseur {
 	//� quel �tage se trouve  l'ascenseur
 	private int etage;
 	//liste des boutons présents sur le panneau interne de l'ascenseur
-	private ArrayList<BoutonInterne> listeBoutons;
+	private ArrayList<BoutonInterne> listeBoutons = new ArrayList<BoutonInterne>();
 	//l'ascenseur est-il en mouvement ou bien � l'arr�t
 	private boolean estEnMouvement;
 	//quel est le poids maximum en kg que l'ascenseur est cens� pouvoir supporter
@@ -28,8 +29,8 @@ public class Ascenseur {
 		estVide = true;				//un nouvel ascenseur ne contient aucun usager
 		poidsMax = 300;				//param�tre par d�faut - � changer ou rendre param�trable par l'utilisateur
 		bat = bati;
-		for (int i = 0; i < bat.getNbEtages(); i++){
-			listeBoutons.add(new BoutonInterne("Etage"+i)); //i = numero de l'etage correspondant au bouton
+		for (int i = 0; i <= bat.getNbEtages(); ++i){
+			listeBoutons.add(new BoutonDestination("Etage"+i, i)); //i = numero de l'etage correspondant au bouton
 		} //initialisation des boutons
 	}
 
