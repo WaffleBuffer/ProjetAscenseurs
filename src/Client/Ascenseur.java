b6e94403cb2da688cf16533e6d409ae8421253c3
m14002147 <m14002147@a-f001817-l.salsa.univ-amu.fr>
2016-01-08 10:57:48 +0100

    Changement Batiment pour stocker les ControleurInterne et le ControleurExterne + Changer le contructeur d'Ascenseur pour ne lui passer plus que le nombre d'etage et le numero

--- src/Client/Ascenseur.java
+++ src/Client/Ascenseur.java
@@ -23,8 +23,6 @@ public class Ascenseur {
 	private int poidsMax;
 	//l'ascenseur est-il vide
 	private boolean estVide;
-	//le batiment dans lequel est l'ascenseur
-	private Batiment bat;
 	//L'ascenseur est-il bloque
 	private boolean estBloque;
 	//numero de l'ascenseur
@@ -32,17 +30,16 @@ public class Ascenseur {
 	
 	
 	//constructeur
-	public Ascenseur (Batiment bati, int num){
+	public Ascenseur (int nbEtage, int num){
 		etage = 0;					//un nouvel ascenseur est assembl� au rez-de-chauss�e (niveau 0)
 		estEnMouvement = false;		//un nouvel ascenseur est immobile car n'a pas encore re�u de requ�te
 		estVide = true;				//un nouvel ascenseur ne contient aucun usager
 		portesOuvertes = false;		//un nouvel acsenseur à les portes fermées
-		poidsMax = 300;				//param�tre par d�faut - � changer ou rendre param�trable par l'utilisateur
-		bat = bati;					
+		poidsMax = 300;				//param�tre par d�faut - � changer ou rendre param�trable par l'utilisateur				
 		numAsc = num;
 		listeBoutons.add(new BoutonDestination("Rez-de-chaussé", 0)); //tout ascenseur à un bouton rez-de chaussé
 		listeBoutons.add(new BoutonDestination("1er étage", 1)); //tout ascenseur à un bouton 1er etage
-		for (int i = 2; i <= bat.getNbEtages(); ++i){
+		for (int i = 2; i <= nbEtage; ++i){
 			listeBoutons.add(new BoutonDestination(i+"e étage", i)); //i est numero de l'etage correspondant au bouton
 		} //initialisation des boutons : autant de boutons qu'il y a d'étages
 		listeBoutons.add(new BoutonStop()); // tout ascenseur à un bouton stop
@@ -52,7 +49,7 @@ public class Ascenseur {
 	public String toString() {
 		return "Ascenseur [etage=" + etage + ", estEnMouvement=" + estEnMouvement + ", portesOuvertes="
 				+ portesOuvertes + ", poidsMax=" + poidsMax + ", estVide="
-				+ estVide + ", bat=" + bat + ", estBloque=" + estBloque
+				+ estVide + ", estBloque=" + estBloque
 				+ ", numAsc=" + numAsc + "]";
 	} // affichage de l'etat de l'ascenseur
 
--- src/Client/Batiment.java
+++ src/Client/Batiment.java
@@ -5,13 +5,21 @@ import java.util.ArrayList;
 import Boutons.BoutonBas;
 import Boutons.BoutonExterne;
 import Boutons.BoutonHaut;
+import Controleurs.ControleurExterne;
+import Controleurs.ControleurInterne;
 
 public class Batiment {
 	private String nom; // le batiment a un nom
 	private int nbEtages; //le batiment a un certain nombre d'etages
-	private ArrayList<BoutonExterne> listeBoutons = new ArrayList<BoutonExterne>(); //le batiment possede une liste de bouton
+	private ControleurExterne controleurExt = ControleurExterne.getControleurExterne();
 	
-	public Batiment(String nom, int nbEtages) {
+	//liste des controleurs internes
+	private ArrayList<ControleurInterne> controleursInt = new ArrayList<ControleurInterne>();
+	
+	//le batiment possede une liste de bouton
+	private ArrayList<BoutonExterne> listeBoutons = new ArrayList<BoutonExterne>();
+	
+	public Batiment(String nom, int nbEtages, int nbAscenseur) {
 		this.nom = nom;
 		this.nbEtages = nbEtages;
 		listeBoutons.add(new BoutonHaut(0)); //le rez de chaussé n'a qu'un bouton haut et as de boton bas
@@ -20,6 +28,14 @@ public class Batiment {
 			listeBoutons.add(new BoutonHaut(i));
 			listeBoutons.add(new BoutonBas(i));
 		} //a chaque étage, le batiment possede deux boutons : haut et bas
+		
+		for (int i = 0; i < nbAscenseur; ++i) {
+			controleursInt.add(new ControleurInterne(new Ascenseur(nbEtages, i)));
+		}
+		
+		//parametrage du controleur Externe
+		controleurExt.defineBatiment (this);
+		controleurExt.defineControleursInterne(controleursInt);
 		
 		listeBoutons.add(new BoutonBas(nbEtages)); // le dernier étage n'a qu'un bouton bas et pas de bouton haut
 	}
@@ -32,6 +48,13 @@ public class Batiment {
 		return nbEtages;
 	}
 	
+	public ControleurExterne getControleurExt () {
+		return controleurExt;
+	}
+	
+	public ArrayList<ControleurInterne> getControleursInterne () {
+		return controleursInt;
+	}
 
 	public ArrayList<BoutonExterne> getListeBoutons() {
 		return listeBoutons;
