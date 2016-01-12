4ea627226f1247a4ea7e34764b32b8a1e1687e8b
Tixp <r.segretain@hotmail.fr>
2016-01-12 15:21:36 +0100

    Ajout de la classe Constantes

---
+++ src/Client/Constantes.java
@@ -0,0 +1,9 @@
+package Client;
+
+public class Constantes {
+	
+	public static final int HAUT = 0;
+	public static final int BAS = 1;
+	public static final int STOP = 2;
+	public static final int DEPLACEMENT = 3;
+}
