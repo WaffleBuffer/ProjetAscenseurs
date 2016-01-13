8e5598238953d6dd012e9d6fb30bee0e9cac9a98
p14005728 <p14005728@a-f002089-l.salsa.univ-amu.fr>
2016-01-13 15:41:48 +0100

    javadoc de Client.Constantes

--- src/Client/Constantes.java
+++ src/Client/Constantes.java
@@ -1,9 +1,29 @@
 package Client;
 
+/**Creation des Constantes utilisees dans {@link Controleurs.ControleurExterne} et {@link Controleurs.ControleurInterne}.
+ * Correspond au {@link Requetes.Requete#libelle} d'une {@link Requetes.Requete}.
+ * @author p14005728
+ * @see Requetes.Requete
+ */
 public abstract class Constantes {
 	
+	/**correspond au {@link Requetes.Requete#libelle} "HAUT" d'une {@link Requetes.Requete}
+	 * 
+	 */
 	public static final int HAUT        = 0;
+	
+	/**correspond au {@link Requetes.Requete#libelle} "BAS" d'une {@link Requetes.Requete}
+	 * 
+	 */
 	public static final int BAS         = 1;
+	
+	/**correspond au {@link Requetes.Requete#libelle} "STOP" d'une {@link Requetes.Requete}
+	 * 
+	 */
 	public static final int STOP        = 2;
+	
+	/**correspond au {@link Requetes.Requete#libelle} "DEPLACEMENT" d'une {@link Requetes.Requete}
+	 * 
+	 */
 	public static final int DEPLACEMENT = 3;
 }
