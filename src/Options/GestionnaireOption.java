bfbaab5264c5dbae0a4db7dd9bfe0f9b97506fde
p14005728 <p14005728@a-f002089-l.salsa.univ-amu.fr>
2016-01-13 13:54:50 +0100

    GestionnaireOption

--- src/Options/GestionnaireOption.java
+++ src/Options/GestionnaireOption.java
@@ -4,20 +4,35 @@ import java.util.ArrayList;
 
 public class GestionnaireOption {
 
+	/** création d'une liste de {@link IOption}
+	 */
 	private ArrayList<IOption> options = new ArrayList<IOption>();
 
+	/** permet l'accès à la liste de {@link IOption}
+	 * @return options liste de {@link IOption}
+	 */
 	public ArrayList<IOption> getOptions() {
 		return options;
 	}
 	
+	/** permet d'ajouter une option à la liste de {@link IOption}
+	 * @param option
+	 */
 	public void addOption (IOption option) {
 		options.add(option);
 	}
 	
+	/** permet d'obtenir le numero de l'option
+	 * @param i numero de l'option
+	 * @return
+	 */
 	public IOption getOption (int i) {
 		return options.get(i);
 	}
 
+	/**permet d'activer une option grace a son numero
+	 * @param numOption numero de l'option
+	 */
 	public void activerOption (int numOption) {
 		//traitement par defaut
 		options.get(numOption).activer();
